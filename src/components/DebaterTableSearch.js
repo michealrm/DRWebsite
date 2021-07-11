68fe23626c53f86ba29cad6790603167209cea3d
Micheal <8922961+michealrm@users.noreply.github.com>
2021-07-11 13:57:32 -0500

    LD table search

--- src/components/DebaterTableSearch.js
+++ src/components/DebaterTableSearch.js
@@ -5,18 +5,28 @@ export const DebaterTableSearch = React.forwardRef((props, ref) => {
     const [tableQuery, setTableQuery] = useState('')
     const [searchResults, setSearchResults] = useState([])
     const [selectedIndex, setSelectedIndex] = useState(-1)
+    const [showResults, setShowResults] = useState(false)
+
+    props.setShowResults(setShowResults)
 
     // When query changes, set searchResult state by calling API
     useEffect(() => {
+        // Separate school from query with comma is matches SCHOOL, [initials]
+        let query = tableQuery
+        const split = query.split(' ')
+        if(split[split.length - 1].length === 2)
+            query = query.substring(0, query.length - 3) + ', ' + query.substring(query.length - 2)
+
+        console.log(query)
         HomePageAPI.get('debaterTableSearch',
-            {params: {query: tableQuery}})
+            {params: {query: query, event: 'LD', season: props.season}})
             .then(response => {
                 setSearchResults(response.data)
             })
     }, [tableQuery])
 
     const renderResults = () => {
-        if(props.showResults) {
+        if(showResults) {
             return searchResults.map((result, index) => {
                 return <li
                     onMouseEnter={() => setSelectedIndex(index)}
@@ -30,6 +40,22 @@ export const DebaterTableSearch = React.forwardRef((props, ref) => {
         }
     }
 
+    const keyHandler = event => {
+        if (event.code === 'ArrowDown' || event.code === 'ArrowUp' || event.code === 'Enter') {
+            event.preventDefault()
+            console.log(event.code + selectedIndex)
+            if (event.code === 'ArrowDown' && selectedIndex < searchResults.length)
+                setSelectedIndex(selectedIndex + 1)
+
+            if (event.code === 'ArrowUp' && selectedIndex > -1)
+                setSelectedIndex(selectedIndex - 1)
+
+            if (event.code === 'Enter' && selectedIndex >= 0 && selectedIndex < searchResults.length)
+                props.gotoEntryCallback(searchResults[selectedIndex].page, searchResults[selectedIndex].index)
+
+        }
+    }
+
     return <div ref={ref}>
         <input
             onChange={(data) => {
@@ -40,7 +66,9 @@ export const DebaterTableSearch = React.forwardRef((props, ref) => {
             }}
             className="form-control" type="text"
             placeholder="Search debaters"
-            aria-label="Table Search" />
+            aria-label="Table Search"
+            onKeyDown={keyHandler}
+        />
 
         <ul style={{position: 'absolute', zIndex: '9'}} className="list-group">
             {renderResults()}
